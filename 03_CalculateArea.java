69bc51a7202cfbe7228b1d4037a64244952de630
Peter Niemeyer <154435338+PNiemeyerMG@users.noreply.github.com>
2024-01-23 14:00:25 -0500

    Add files via upload

---
+++ 03_CalculateArea.java
@@ -0,0 +1,14 @@
+/* 
+Name: Niemeyer
+Date: Spring '24
+Purpose: This program computes the area of a circle
+*/
+public class ComputeArea {
+  public static void main(String[] args) {
+    double radius;
+    double area;
+    // Step 1: Read in radius
+    // Step 2: Compute area
+    // Step 3: Display the area
+  }
+}
