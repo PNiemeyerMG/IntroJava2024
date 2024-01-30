6bd80c6c0639d2e428e0b4b6eb7beedfaadf9e49
Peter Niemeyer <154435338+PNiemeyerMG@users.noreply.github.com>
2024-01-30 09:39:21 -0500

    Delete 04_Error_Correction.java

--- 04_Error_Correction.java
+++
@@ -1,8 +0,0 @@
-class Main { 
-    // compute the area of a circle
-    public static void main(String [] args)
-      radius = 6;
-      pi = 3.14;
-      System.out.println("Circumference: " + pi * 2 * radius);
-    }
-  }
