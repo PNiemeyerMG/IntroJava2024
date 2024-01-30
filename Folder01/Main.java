fb5a0aeedd8f2959df9b6bd4543cf2fa7b192d5b
ScatterBrain-I <adalwulf23@gmail.com>
2024-01-30 09:36:40 -0500

    add new

---
+++ Folder01/Main.java
@@ -0,0 +1,8 @@
+class Main { 
+    // compute the area of a circle
+    public static void main(String [] args)
+      radius = 6;
+      pi = 3.14;
+      System.out.println("Circumference: " + pi * 2 * radius);
+    }
+  }
---
+++ Folder01/Welcome.java
@@ -0,0 +1,7 @@
+// This program prints "Welcome to Java I!"
+// This is a comment and it doesn't impact the program
+class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java I!");
+    }
+}
