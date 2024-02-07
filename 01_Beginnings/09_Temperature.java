5a51572aeeb39a86130ed6ff5a589ae90ee938f0
Peter Niemeyer <154435338+PNiemeyerMG@users.noreply.github.com>
2024-02-07 13:35:23 -0500

    Update and rename 09_Fahrenheit.java to 09_Temperature.java

---
+++ 01_Beginnings/09_Temperature.java
@@ -0,0 +1,9 @@
+// Converts Fahrenheit to Celsius
+class Fahrenheit {
+    public static void main(String[] args) {
+        // Declare two variables 'fahrenheit' and 'celsius
+        // Convert with yhe following formula:
+        // Subtract 32 then multiply by 5 / 9
+        // Print the result ina statement hat is clear to the user
+    }
+}
